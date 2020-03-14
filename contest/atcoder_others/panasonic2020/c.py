8b56a11e85851f44040e3d2a9e7d26e29885c52e
arakoma <arakoma555@gmail.com>
2020-03-15 01:45:46 +0900

    add panasonic2020

---
+++ contest/atcoder_others/panasonic2020/c.py
@@ -0,0 +1,12 @@
+a, b, c = map(int, input().split())
+
+if c - a - b > 0:
+    if 4 * a * b < (c - a - b) ** 2:
+        ans = "Yes"
+    else:
+        ans = "No"
+else:
+    ans = "No"
+
+
+print(ans)
